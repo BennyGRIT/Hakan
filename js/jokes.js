350f0893efd18ab40cf4dd84a760e5e459bca13d
BennyGRIT <benny.andersson@gritacademy.se>
2022-06-12 21:30:56 +0200

    testing touch for mobile

--- js/jokes.js
+++ js/jokes.js
@@ -1,7 +1,22 @@
 let theJokeButton = document.getElementById('theJokeButton');
 let content = document.getElementById('content');
 
-theJokeButton.onclick = function (event) {
+// theJokeButton.addEventListener = function (event) {
+//     event.preventDefault();
+//     content.textContent = ""
+//     const randomNumber = Math.floor(Math.random() * 14);
+//     console.log(randomNumber)
+
+//     showJokes().then(response => {
+//         let thisP = document.createElement('p');
+//         content.append(thisP);
+//         thisP.setAttribute('id', 'pjoke')
+//         thisP.textContent += randomNumber+1 + ". " + response[randomNumber].theJoke + ' '
+//     });
+// };
+
+
+theJokeButton.addEventListener('click', function(event){
     event.preventDefault();
     content.textContent = ""
     const randomNumber = Math.floor(Math.random() * 14);
@@ -13,7 +28,12 @@ theJokeButton.onclick = function (event) {
         thisP.setAttribute('id', 'pjoke')
         thisP.textContent += randomNumber+1 + ". " + response[randomNumber].theJoke + ' '
     });
-};
+});
+
+
+
+
+
 async function showJokes() {
     let response = await fetch('http://localhost:4559/showJokes');
     let data = await response.json();
